aa286ec75c020227107b6896d8429f429e976db1
DeevDevs <dmitriy.vnuchkov@gmail.com>
2022-12-18 20:06:09 +0400

    fix: modify Store and one component

--- src/pages/store/Store.tsx
+++ src/pages/store/Store.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import CategorySearchBlock from '../../components/categorySearchBlock/CategorySearchBlock';
+import CategorySearchBlock from '../../components/CategorySearchBlock/CategorySearchBlock';
 import SearchField from '../../components/seachField/SearchField';
 import SearchParams from '../../components/searchParams/SearchParams';
 import SearchViewParams from '../../components/searchViewParams/SearchViewParams';
 import SortViewBlock from '../../components/sortViewBlock/SortViewBlock';
-import SearchParams from '../../components/searchParams/SearchParams';
 import './store.scss';
 
 function Store() {
